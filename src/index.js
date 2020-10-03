b0501f823b82fdb0a30f21a7d6ee619b1ecec4be
Андрей <dronxxx1993@yandex.ru>
2020-10-03 23:01:05 +0400

    feat : test solution arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) {
+        return 0
+    } else {
+        let z = array[0];
+        for(let i = 0;i<array.length;i++){
+            if(array[i] < z){
+                z = array[i]
+            }
+        }
+        return z
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) {
+        return 0
+    } else {
+        let z = 0;
+        for(let i = 0;i<array.length;i++){
+            if(array[i] > z){
+                z = array[i]
+            }
+        }
+        return z
+    }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    if (array == undefined || array.length == 0) {
+        return 0
+    } else {
+        return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0) / array.length
+    }
+
 }
+
